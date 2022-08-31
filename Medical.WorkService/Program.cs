9a845d3fcaab062a3b99b3175c10e67d3b642441
superfeeling <superfeeling@126.com>
2022-08-31 10:43:54 +0800

    web项目windows服务部署

---
+++ Medical.WorkService/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Medical.WorkService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args);
+
+            //判断当前系统是否为windows
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                host.UseWindowsService();
+            }
+            return host.ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<Worker>();
+            }).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+        }
+            
+    }
+}
